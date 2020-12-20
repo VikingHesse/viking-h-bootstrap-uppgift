3ceb3b13081ba5de9d20392590ec6eab8eb5d983
VikingHesse <70264379+VikingHesse@users.noreply.github.com>
2020-12-20 01:44:09 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+}
