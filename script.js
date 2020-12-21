cf20f63255128433178e289fda88a215e459c904
VikingHesse <70264379+VikingHesse@users.noreply.github.com>
2020-12-21 20:17:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+/* Är inte min javascript så kan inte förklara det helt, men jag kan försöka*/
+
+/* sparar elementet från knappens ID (myBtn) i "mybutton"*/
 mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
-
+/* Denna funktionen körs när man klickar på knappen, den ser till så man skrollas upp till toppen av sidan. */
 function topFunction() {
     document.body.scrollTop = 0; 
     document.documentElement.scrollTop = 0;
